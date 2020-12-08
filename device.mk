ce2ae58215f902d2da45b46b3c1024fc2b7e03ab
Bruno Martins <bgcngm@gmail.com>
2020-12-08 22:03:48 +0000

    FP5: Build Health 2.1 HAL
    Change-Id: Ic6302844ac58c16713b43b6871a705b9b4749a96

--- device.mk
+++ device.mk
@@ -212,6 +212,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
 # Init
 PRODUCT_PACKAGES += \
     init.bt_mac.sh \
