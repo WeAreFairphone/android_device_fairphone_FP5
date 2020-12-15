8d4b8b14d539de271e390385a30c9c819d464622
Volodymyr Zhdanov <wight554@gmail.com>
2020-12-15 21:30:24 +0100

    FP5: Build vendor.qti.hardware.servicetracker@1.2 into /vendor

--- device.mk
+++ device.mk
@@ -357,6 +357,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+# QTI service tracker
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetracker@1.2.vendor
+
 # Shipping API 
 PRODUCT_SHIPPING_API_LEVEL := 33
 
