75affa25ca864d3bdeaf5c58116598405abd1aeb
LuK1337 <priv.luk@gmail.com>
2021-12-14 11:42:44 +0100

    FP5: Build android.hardware.gatekeeper@1.0 into vendor
    * Required by the proprietary gatekeeper blobs
    
    Change-Id: I1db79176ac103cb7fc8db0fa4b4cfc699e38ad49

--- device.mk
+++ device.mk
@@ -190,6 +190,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0.vendor
+
 # Init
 PRODUCT_PACKAGES += \
     init.bt_mac.sh \
