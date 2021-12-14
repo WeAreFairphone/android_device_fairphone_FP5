2171ddf6e2d1e22ac37f6b0025aba2e884f605d6
LuK1337 <priv.luk@gmail.com>
2021-12-14 11:42:44 +0100

    FP5: Build android.hardware.drm@1.3 into vendor
    * Required by the proprietary widevine DRM blobs
    
    Change-Id: I1db79176ac103cb7fc8db0fa4b4cfc699e38ad49

--- device.mk
+++ device.mk
@@ -170,6 +170,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.3.vendor
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
