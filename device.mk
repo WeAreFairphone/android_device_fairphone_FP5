397f1f8df5782058ee096029ca6dfe264860ad24
Rohit Sekhar <merothh@e.email>
2023-09-11 11:54:48 +0530

    FP5: Build packages/perms for GPS stack

--- device.mk
+++ device.mk
@@ -194,6 +194,15 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
+# GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss.measurement_corrections@1.1.vendor \
+    android.hardware.gnss.visibility_control@1.0.vendor \
+    android.hardware.gnss@2.1.vendor
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
+
 # Init
 PRODUCT_PACKAGES += \
     init.bt_mac.sh \
