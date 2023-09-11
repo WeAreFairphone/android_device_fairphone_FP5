1f19479940a3ff36c2a37707344adb08659b0db1
Rohit Sekhar <merothh@e.email>
2023-09-11 12:30:17 +0530

    FP5: Build packages for USB

--- device.mk
+++ device.mk
@@ -399,6 +399,17 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service-qti
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
+
 # Kernel
 $(call inherit-product, device/fairphone/FP5-kernel/kernel.mk)
 
