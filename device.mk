79d567f123174e883b56abf4ea31f9bbd3a7fe75
Rohit Sekhar <merothh@e.email>
2023-09-11 12:47:32 +0530

    FP5: Build Vibrator HAL

--- device.mk
+++ device.mk
@@ -418,6 +418,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
 # Kernel
 $(call inherit-product, device/fairphone/FP5-kernel/kernel.mk)
 
