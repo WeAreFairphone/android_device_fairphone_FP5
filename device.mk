637a2e631cf59066986c7c0275925522b0f91daf
Rohit Sekhar <merothh@e.email>
2023-09-11 12:56:03 +0530

    FP5: Build packages for WiFi Display

--- device.mk
+++ device.mk
@@ -443,6 +443,14 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libnl \
+    libwfdaac_vendor
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # Kernel
 $(call inherit-product, device/fairphone/FP5-kernel/kernel.mk)
 
