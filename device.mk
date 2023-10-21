d7c01c853ce7c7bbcabe4c2f5b5d0a2c89a3638e
LuK1337 <priv.luk@gmail.com>
2023-10-21 16:34:40 +0200

    FP5: Build android.frameworks.sensorservice@1.0.vendor
    Required by mm-pp-deps.
    
    Change-Id: Iaf821dcbb07d7820f2fc3ff5ca9cca5e973360f2

--- device.mk
+++ device.mk
@@ -363,6 +363,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.0-service.multihal
 
 PRODUCT_COPY_FILES += \
