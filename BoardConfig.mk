2ea15ecf66f591f9e67a32e731f6a1d2b2cc8591
Rohit Sekhar <git@merothh.com>
2023-11-02 11:09:30 +0530

    FP5: Bump to FP5.TT3P.A.112.20231016

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,7 +185,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security
-VENDOR_SECURITY_PATCH := 2023-09-05
+VENDOR_SECURITY_PATCH := 2023-10-05
 
 # SELinux
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
